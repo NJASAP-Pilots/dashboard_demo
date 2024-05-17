748e171ab7319ea3e3427daf4627dbb2a4cba4f9
Paul Flynn <paul@MBP20.localdomain>
2024-05-17 18:38:54 -0400

    New calendar tab layout

--- 8_6_calendar_loop.R
+++ 8_6_calendar_loop.R
@@ -3,11 +3,11 @@ library(calendR)
 library(glue)
 library(readxl)
 
-t2024_77_lines <- read_excel("~/Library/CloudStorage/OneDrive-Personal/Documents/R Studio/Schedule Calendars/2024-04-04 R 7-7 Calendar.xlsx", 
-                           sheet = "CalendR Days 2024",
-                           range = cell_cols(31:43))
+t2024_86_lines <- read_excel("~/Library/CloudStorage/OneDrive-Personal/Documents/R Studio/Schedule Calendars/2024-04-04 R 7-7 Calendar.xlsx", 
+                             sheet = "CalendR Days 2024 8_6",
+                             range = cell_cols(16:28))
 
-var_list <- t2024_77_lines %>% 
+var_list <- t2024_86_lines %>% 
   select(1:13) %>% 
   colnames()
 
@@ -16,42 +16,42 @@ for (i in 1:13) {
   
   # Variables
   
-  line_data <- unlist(t2024_77_lines[i])
+  line_data <- unlist(t2024_86_lines[i])
   line <- var_list[i]
   line_legend <- sub("_"," ",line)
   line_no <- ifelse(nchar(line) == 7, str_sub(line,-2), str_sub(line,-1))
   cal_title <- glue("2024 7&7-{line_no}")
-  save_as <- glue("7_7_{line}.png")
-
-imp.dates <- rep(NA, 366)
-imp.dates[line_data] <- line_legend
-imp.dates[c(1,15,50,89,91,148,186,246,333,360)] <- "NJA Holidays"
-
-calendR(year = 2024, ncol = 1, # Year
-        title = cal_title,
-        mbg.col = "steelblue",           # Background color for the month names
-        months.pos = 0.5,    # Horizontal alignment of the month names
-        months.col = "white",  # Text color of the month names
-        special.days = imp.dates,    # Color days of the year
-        special.col = c("lightblue", "pink"), # Color of the special.days
-        legend.pos = "bottom")
-
-ggsave(save_as, path = "images/2024_prod",
-       width = 720,
-       height = 4000,
-       units =c("px"),
-       device = png,
-       dpi = 240
-       )
+  save_as <- glue("8_6_{line}.png")
+  
+  imp.dates <- rep(NA, 366)
+  imp.dates[line_data] <- line_legend
+  imp.dates[c(1,15,50,89,91,148,186,246,333,360)] <- "NJA Holidays"
+  
+  calendR(year = 2024, ncol = 1, # Year
+          title = cal_title,
+          mbg.col = "steelblue",           # Background color for the month names
+          months.pos = 0.5,    # Horizontal alignment of the month names
+          months.col = "white",  # Text color of the month names
+          special.days = imp.dates,    # Color days of the year
+          special.col = c("lightblue", "pink"), # Color of the special.days
+          legend.pos = "bottom")
+  
+  ggsave(save_as, path = "images/2024_prod",
+         width = 720,
+         height = 4000,
+         units =c("px"),
+         device = png,
+         dpi = 240
+  )
 }
 
 ### 2025 ###
 
-t2025_77_lines <- read_excel("~/Library/CloudStorage/OneDrive-Personal/Documents/R Studio/Schedule Calendars/2024-04-04 R 7-7 Calendar.xlsx", 
-                             sheet = "CalendR Days 2025",
+t2025_86_lines <- read_excel("~/Library/CloudStorage/OneDrive-Personal/Documents/R Studio/Schedule Calendars/2024-04-04 R 7-7 Calendar.xlsx", 
+                             sheet = "CalendR Days 2025 8_6",
                              range = cell_cols(18:30))
 
-var_list <- t2024_77_lines %>% 
+var_list <- t2024_86_lines %>% 
   select(1:13) %>% 
   colnames()
 
@@ -60,12 +60,12 @@ for (i in 1:13) {
   
   # Variables
   
-  line_data <- unlist(t2025_77_lines[i])
+  line_data <- unlist(t2025_86_lines[i])
   line <- var_list[i]
   line_legend <- sub("_"," ",line)
   line_no <- ifelse(nchar(line) == 7, str_sub(line,-2), str_sub(line,-1))
-  cal_title <- glue("2025 7&7-{line_no}")
-  save_as <- glue("7_7_{line}.png")
+  cal_title <- glue("2025 8&6-{line_no}")
+  save_as <- glue("8_6_{line}.png")
   
   imp.dates <- rep(NA, 365)
   imp.dates[line_data] <- line_legend
@@ -90,10 +90,10 @@ for (i in 1:13) {
 }
 
 ----------------------------------------------------------------------
-# 2024-05-15 2302
-    
-imp.dates <- rep(NA, 397)
-imp.dates[t2024_77_lines$Line_2] <- "Line 2"
+  # 2024-05-15 2302
+  
+  imp.dates <- rep(NA, 397)
+imp.dates[t2024_86_lines$Line_2] <- "Line 2"
 imp.dates[c(1,15,50,89,91,148,186,246,333,360)] <- "NJA Holidays"
 
 calendR(from = "2024-01-01",
--- scratch.R
+++ scratch.R
@@ -1,3 +1,226 @@
+# Schedules (Mobile) {scrolling="true"}
+
+## row {.tabset}
+
+### 7&7
+
+#### column {.tabset}
+
+###### Line 2
+![](images/2024_prod/7_7_Line_2.png)
+
+###### 3
+![](images/2024_prod/7_7_Line_3.png)
+
+###### 4
+![](images/2024_prod/7_7_Line_4.png)
+
+###### 5
+![](images/2024_prod/7_7_Line_5.png)
+
+###### 6
+![](images/2024_prod/7_7_Line_6.png)
+
+###### 7
+![](images/2024_prod/7_7_Line_7.png)
+
+###### 9
+![](images/2024_prod/7_7_Line_9.png)
+
+###### 10
+![](images/2024_prod/7_7_Line_10.png)
+
+###### 11
+![](images/2024_prod/7_7_Line_11.png)
+
+###### 12
+![](images/2024_prod/7_7_Line_12.png)
+
+###### 13
+![](images/2024_prod/7_7_Line_13.png)
+
+###### 14
+![](images/2024_prod/7_7_Line_14.png)
+
+#### column {.tabset}
+
+##### 2025 7&7 Line 2
+![](images/2025_prod/7_7_Line_2.png)
+
+##### 3
+![](images/2025_prod/7_7_Line_3.png)
+
+##### 4
+![](images/2025_prod/7_7_Line_4.png)
+
+##### 5
+![](images/2025_prod/7_7_Line_5.png)
+
+##### 6
+![](images/2025_prod/7_7_Line_6.png)
+
+##### 7
+![](images/2025_prod/7_7_Line_7.png)
+
+##### 9
+![](images/2025_prod/7_7_Line_9.png)
+
+##### 10
+![](images/2025_prod/7_7_Line_10.png)
+
+##### 11
+![](images/2025_prod/7_7_Line_11.png)
+
+##### 12
+![](images/2025_prod/7_7_Line_12.png)
+
+##### 13
+![](images/2025_prod/7_7_Line_13.png)
+
+##### 14
+![](images/2025_prod/7_7_Line_14.png)
+
+### 8&6
+
+#### column {.tabset}
+
+##### Line 12
+![](images/2024_prod/7_7_Line_12.png)
+
+##### Line 13
+![](images/2024_prod/7_7_Line_13.png)
+
+---------------------
+
+
+
+## column {width="50%"}
+
+### row {.tabset}
+
+#### 2024 7&7 Line 2
+![](images/2024_prod/7_7_Line_2.png)
+
+#### 3
+![](images/2024_prod/7_7_Line_3.png)
+
+#### 4
+![](images/2024_prod/7_7_Line_4.png)
+
+#### 5
+![](images/2024_prod/7_7_Line_5.png)
+
+#### 6
+![](images/2024_prod/7_7_Line_6.png)
+
+#### 7
+![](images/2024_prod/7_7_Line_7.png)
+
+#### 9
+![](images/2024_prod/7_7_Line_9.png)
+
+#### 10
+![](images/2024_prod/7_7_Line_10.png)
+
+#### 11
+![](images/2024_prod/7_7_Line_11.png)
+
+#### 12
+![](images/2024_prod/7_7_Line_12.png)
+
+#### 13
+![](images/2024_prod/7_7_Line_13.png)
+
+#### 14
+![](images/2024_prod/7_7_Line_14.png)
+
+### row {.tabset}
+
+#### 2024 8&6 Line 2
+![](images/2024_prod/7_7_Line_2.png)
+
+#### 3
+![](images/2024_prod/7_7_Line_3.png)
+
+#### 4
+![](images/2024_prod/7_7_Line_4.png)
+
+#### 5
+![](images/2024_prod/7_7_Line_5.png)
+
+#### 6
+![](images/2024_prod/7_7_Line_6.png)
+
+#### 7
+![](images/2024_prod/7_7_Line_7.png)
+
+#### 9
+![](images/2024_prod/7_7_Line_9.png)
+
+#### 10
+![](images/2024_prod/7_7_Line_10.png)
+
+#### 11
+![](images/2024_prod/7_7_Line_11.png)
+
+#### 12
+![](images/2024_prod/7_7_Line_12.png)
+
+#### 13
+![](images/2024_prod/7_7_Line_13.png)
+
+#### 14
+![](images/2024_prod/7_7_Line_14.png)
+
+## column {width="50%"}
+
+### row {.tabset height="100%"}
+
+#### 2025 7&7 Line 2
+![](images/2025_prod/7_7_Line_2.png)
+
+#### 3
+![](images/2025_prod/7_7_Line_3.png)
+
+#### 4
+![](images/2025_prod/7_7_Line_4.png)
+
+#### 5
+![](images/2025_prod/7_7_Line_5.png)
+
+#### 6
+![](images/2025_prod/7_7_Line_6.png)
+
+#### 7
+![](images/2025_prod/7_7_Line_7.png)
+
+#### 9
+![](images/2025_prod/7_7_Line_9.png)
+
+#### 10
+![](images/2025_prod/7_7_Line_10.png)
+
+#### 11
+![](images/2025_prod/7_7_Line_11.png)
+
+#### 12
+![](images/2025_prod/7_7_Line_12.png)
+
+#### 13
+![](images/2025_prod/7_7_Line_13.png)
+
+#### 14
+![](images/2025_prod/7_7_Line_14.png)
+
+
+
+
+install.packages("maps")
+install.packages("mapproj")
+
+library(tidyverse)
+library(maps)
+library(mapproj)
 
 sessionInfo()
 
